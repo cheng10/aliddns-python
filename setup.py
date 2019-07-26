05f68c8f5e87d19067795842317e94dc53cf396c
cheng10 <cheng10@ualberta.ca>
2019-07-26 14:37:12 +0800

    update doc & setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cheng10/aliddns-python",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'docopt',
+        'requests',
+        'aliyun-python-sdk-core',
+        'aliyun-python-sdk-alidns',
+    ],
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
